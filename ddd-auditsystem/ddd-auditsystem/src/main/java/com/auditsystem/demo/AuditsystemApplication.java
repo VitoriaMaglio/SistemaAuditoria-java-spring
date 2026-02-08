d51323a93dfcc09f46a022dbc49a4263e965a3cd
VitoriaMaglio <vitoriamaglio@gmail.com>
2026-02-07 23:07:38 -0300

    Criando projeto com spring boot.

---
+++ ddd-auditsystem/ddd-auditsystem/src/main/java/com/auditsystem/demo/AuditsystemApplication.java
@@ -0,0 +1,13 @@
+package com.auditsystem.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditsystemApplication.class, args);
+	}
+
+}
