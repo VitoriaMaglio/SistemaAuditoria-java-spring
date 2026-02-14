a40361c71e963b59bba7738fc9e512caf04eb4ab
VitoriaMaglio <vitoriamaglio@gmail.com>
2026-02-14 14:27:27 -0300

    Inicio projeto- mapeando entidade user.

--- auditsystem/src/main/java/com/project/auditsystem/entity/User.java
+++ auditsystem/src/main/java/com/project/auditsystem/entity/User.java
@@ -1,7 +1,31 @@
 package com.project.auditsystem.entity;
 
+import jakarta.persistence.*;
+import jakarta.transaction.Transaction;
 
-    public class User {
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "created_at", nullable = false, updatable = false )
+    private Instant createdAt;
+
+    @OneToMany(mappedBy = "user")
+    private List<Transaction> transactions = new ArrayList<>();
 
 
     }
