5509a123bffac608d654bccba0517beae2f77af7
FelipeMaglio <felipemagliofilho@gmail.com>
2026-02-14 23:10:44 -0300

    Mapeando entidade user e transaction, inserindo comentários para java doc.

---
+++ auditsystem/src/main/java/com/project/auditsystem/entity/Transaction.java
@@ -0,0 +1,51 @@
+package com.project.auditsystem.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+/**
+ * Classe que representa uma transação financeira registrada no sistema e utilizada como base para auditoria.
+ */
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    /**
+     * Identificador único de transação.
+     * Sequence gerade automaticamente pelo banco de dados.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Representa a descrição da transação.
+     */
+    @Column(nullable = false, length = 250)
+    private String description;
+
+    /**
+     * Representa o valor da transação.
+     */
+    @Column(nullable = false,precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    /**
+     * Representa a data/hora em UTC em que a transação foi registrada no sistema..
+     */
+    //Quando o atributo não tiver o mesmo nome da coluna na tabela, mapear com o nome da tabela
+    @Column(name = "created_at" , nullable = false, updatable = false)
+    private Instant createdAt;
+
+    /**
+     *Transações dependentes de um usuário.
+     *Lado forte do relacionamento entre User e Transaction (1-N).
+     */
+    //Lado da relação com fk, esse "user" é o que está mapeado na entidade User indicando o outro lado da relação
+    //esse "user_id" indica a coluna na tabela que tem a fk
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
--- auditsystem/src/main/java/com/project/auditsystem/entity/User.java
+++ auditsystem/src/main/java/com/project/auditsystem/entity/User.java
@@ -1,29 +1,49 @@
 package com.project.auditsystem.entity;
 
 import jakarta.persistence.*;
-import jakarta.transaction.Transaction;
-
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe que representa um usuário do sistema de auditoria e versionamento de dados.
+ */
 @Entity
 @Table(name = "users")
 public class User {
 
+    /**
+     * Identificador único de usuário.
+     * Sequence gerade automaticamente pelo banco de dados.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Representa o nome do usuário no sistema
+     */
     @Column(nullable = false, length = 150)
     private String name;
 
+    /**
+     * Representa o email do usuário no sistema
+     */
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
+    /**
+     * Representa a data/hora em UTC em que o usuário foi registrado no sistema.
+     */
     @Column(name = "created_at", nullable = false, updatable = false )
     private Instant createdAt;
 
+    /**
+     * Transações associadas ao usuário.
+     *Lado fraco do relacionamento entre User e Transaction (1-N).
+     */
+    //Relacionamento com transaction, uma lista para facilitar para o java a dependência
+    //MappedBy sempre indica o lado que possui a fk
     @OneToMany(mappedBy = "user")
     private List<Transaction> transactions = new ArrayList<>();
 
