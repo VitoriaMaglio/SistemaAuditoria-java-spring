90962ee5ec6c5e52ca351aa50befdfd3e11bdfc0
VitoriaMaglio <vitoriamaglio@gmail.com>
2026-02-22 12:56:56 -0300

    Test unitário para método create de service user e transaction.

---
+++ auditsystem/src/test/java/com/project/auditsystem/service/TransactionServiceTest.java
@@ -0,0 +1,118 @@
+package com.project.auditsystem.service;
+
+import com.project.auditsystem.dto.request.TransactionRequestDTO;
+import com.project.auditsystem.dto.response.TransactionResponseDTO;
+import com.project.auditsystem.entity.Transaction;
+import com.project.auditsystem.entity.User;
+import com.project.auditsystem.exception.UserInactiveException;
+import com.project.auditsystem.repository.TransactionRepository;
+import com.project.auditsystem.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+class TransactionServiceTest {
+
+    @Mock
+    private TransactionRepository transactionRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private  AuditLogService auditLogService;
+    @Mock
+    private AlertService alertService;
+
+    @Autowired
+    @InjectMocks
+    private TransactionService transactionService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @DisplayName("Should create transaction successfully")
+    void createTransactionCase1() {
+        //Arrange
+        //passar dados controller
+        //retorna entidade banco
+        //simular existencia
+        User user = new User();
+        String email = "user@email.com";
+        user.setId(1L);
+        user.setActive(true);
+        TransactionRequestDTO requestDTO = new TransactionRequestDTO("Descrição", new BigDecimal("2.00"));
+        Transaction transactionSaved = new Transaction();
+        transactionSaved.setId(10L);
+        transactionSaved.setUser(user);
+
+
+        when(userRepository.findByEmail(email))
+                .thenReturn(Optional.of(user));
+
+        when(transactionRepository.save(any(Transaction.class)))
+                .thenReturn(transactionSaved);
+
+
+        //act
+        TransactionResponseDTO response =
+                transactionService.createTransaction(requestDTO, email);
+
+        //Assert
+        assertNotNull(response);
+
+        verify(userRepository).findByEmail(email);
+        verify(transactionRepository).save(any(Transaction.class));
+
+        verify(alertService).createAlert(user, transactionSaved);
+        verify(alertService).createAlertDes(user, transactionSaved);
+
+        verify(auditLogService).logAction(
+                eq("CREATED"),
+                eq("Transaction"),
+                isNull(),                // ID ainda pode ser null
+                isNull(),
+                eq("Transação criada"),
+                eq(user)
+        );
+
+
+    }
+
+    @Test
+    @DisplayName("Should throw exception UserInactiveException ")
+    void createTransactionCase2() {
+        // Arrange
+        String email = "user@email.com";
+
+        User user = new User();
+        user.setActive(false);
+
+        when(userRepository.findByEmail(email))
+                .thenReturn(Optional.of(user));
+
+        // Act + Assert
+        assertThrows(
+                UserInactiveException.class,
+                () -> transactionService.createTransaction(
+                        new TransactionRequestDTO("Descrição", new BigDecimal("2.00")), email
+                )
+        );
+
+        verify(userRepository).findByEmail(email);
+        verify(transactionRepository, never()).save(any());
+        verifyNoInteractions(alertService, auditLogService);
+    }
+}
--- auditsystem/src/test/java/com/project/auditsystem/service/UserServiceTest.java
+++ auditsystem/src/test/java/com/project/auditsystem/service/UserServiceTest.java
@@ -1,35 +1,115 @@
 package com.project.auditsystem.service;
 
+import com.project.auditsystem.dto.request.UserRequestDTO;
+import com.project.auditsystem.dto.response.UserResponseDTO;
+import com.project.auditsystem.entity.User;
+import com.project.auditsystem.exception.RegisteredEmailException;
 import com.project.auditsystem.repository.UserRepository;
 import com.project.auditsystem.repository.VersionedEntityRepository;
 import com.project.auditsystem.service.mapper.UserSnapshotBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
     @Mock
     private  UserRepository userRepository;
-
+    @Mock
     private  AuditLogService auditLogService;
+    @Mock
     private  AlertService alertService;
+    @Mock
     private VersionedEntityService versionedEntityService;
+    @Mock
     private  UserSnapshotBuilder userSnapshotBuilder;
+    @Mock
     private  VersionedEntityRepository versionedEntityRepository;
+
+    @Autowired
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
 
     @Test
     @DisplayName("Should create user successfully" )
     void createUserCase1() {
+
+        /// AAA
+        //Arrange
+        //DTO simula o que o controller enviaria
+        UserRequestDTO requestDTO = new UserRequestDTO("User","teste@email.com","Senah123");
+        //Entidade simula o retorno do banco
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("teste@email.com");
+
+        //Simula que o emial ainda n existe
+        when(userRepository.existsByEmail("teste@email.com"))
+                .thenReturn(false);
+        //Evita acesso ao banco real
+        when(userRepository.save(any(User.class)))
+                .thenReturn(user);
+
+        //Act
+        UserResponseDTO response = userService.createUser(requestDTO);
+
+        //Assert
+        //Garante que o método não retornou null
+        assertNotNull(response);
+        //Confirma que dado funcionou
+        assertEquals("teste@email.com", response.getEmail());
+
+        verify(userRepository).existsByEmail("teste@email.com");
+        verify(userRepository).save(any(User.class));
+        //Garante regra do sistema
+        verify(auditLogService).logAction(
+                eq("CREATE"),
+                eq("User"),
+                isNull(),
+                isNull(),
+                eq("Usuário criado"),
+                any(User.class)
+        );
+        //Garante regra do sistema
+        verify(alertService).createAlertUser(user);
 
     }
 
     @Test
     @DisplayName("Should throw exception RegisteredEmailException" )
     void createUserCase2() {
-    }
 
+        /// AAA
+        //Arrange
+        //DTO simula o que o controller enviaria
+        UserRequestDTO requestDTO = new UserRequestDTO("User","teste@email.com","Senah123");
+
+        //Simula que o emial ainda n existe
+        when(userRepository.existsByEmail("teste@email.com"))
+                .thenReturn(true);
+
+        //Act
+        assertThrows(
+                RegisteredEmailException.class,
+                () -> userService.createUser(requestDTO));
+        //Assert
+        verify(userRepository).existsByEmail("teste@email.com");
+        verify(userRepository, never()).save(any());
+        verifyNoInteractions(auditLogService, alertService);
+    }
 
 }
